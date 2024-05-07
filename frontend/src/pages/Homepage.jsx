5f58e1d053aef3e0d4897094bac5a84552fdb2c6
Hiba Anwar <hiba2anwar@gmail.com>
2024-05-07 18:20:21 +0400

    Define CSS custom properties
    - Fix import error in homepage.jsx file

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,10 +1,11 @@
 import Navbar from "../layout/Navbar";
+import Footer from "../layout/Footer";
 
 function Homepage() {
 	return (
 		<>
 			<Navbar />
-			<main>
+			<main className={styles.homepage}>
 				<h1>Homepage</h1>
 			</main>
 			<Footer />
