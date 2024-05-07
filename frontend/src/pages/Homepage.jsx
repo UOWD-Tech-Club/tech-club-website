731d3d2040c52a755d0b7df29e89f34db461a571
Hiba Anwar <hiba2anwar@gmail.com>
2024-05-07 17:39:37 +0400

    feat: Initialize project structure and clean up
    - Set up project directory structure for app
    - Remove unnecessary files generated by initial project creation
    - Add initial components for webpage

---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../layout/Navbar";
+
+function Homepage() {
+	return (
+		<>
+			<Navbar />
+			<main>
+				<h1>Homepage</h1>
+			</main>
+			<Footer />
+		</>
+	);
+}
+
+export default Homepage;
