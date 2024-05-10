f27b16ea71045b89e572d32c83cb7556bfa83393
Hiba Anwar <hiba2anwar@gmail.com>
2024-05-10 17:24:20 +0400

    fix: display hero component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 	return (
 		<>
 			{/* <Homepage /> */}
-			{/* <Hero /> */}
+			<Hero />
 			<Events />
 			<Newsfeed />
 		</>
--- frontend/src/layout/Navbar/Navbar.jsx
+++ frontend/src/layout/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Navbar.css";
-import { navLogo } from "../../assets";
+import navLogo from "../../assets/nav-logo.svg";
 
 const Navbar = () => {
 	return (
