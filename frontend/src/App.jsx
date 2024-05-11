ba85cf7581759ff05893201b832c0790adcb610d
Hiba Anwar <hiba2anwar@gmail.com>
2024-05-11 21:51:36 +0400

    feat: navbar responsiveness
    - Add sidebar functionality on tablets and mobile screens
    - Scale according to screen sizes
    - Minor hero fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./App.css";
 import Homepage from "./pages/Homepage";
+import Navbar from "./layout/Navbar";
+import Footer from "./layout/Footer";
 
 function App() {
 	return (
 		<>
-			<Homepage />
+			<BrowserRouter>
+				<Navbar />
+				<Routes>
+					<Route path='/' element={<Homepage />} />
+				</Routes>
+				<Footer />
+			</BrowserRouter>
 		</>
 	);
 }
