bc260cf951534d58cce1dc516da5411a75777fc7
Windsmith <shriramsekar11@gmail.com>
2024-05-19 23:12:27 +0400

    fix: hacky fix 3 for image: hardcoding

--- frontend/src/components/GESection/GECard.jsx
+++ frontend/src/components/GESection/GECard.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 function GECard({ dateTime, title, img, tags, link }) {
 	const imageStyle = {
-		background: `linear-gradient(180deg, rgba(0, 204, 164, 0) 22.62%, #00cca4 99.93%), url("tech-club-website/Event-1.png") lightgray -165.501px 0px / 224.563% 100.387%`,
+		background: `linear-gradient(180deg, rgba(0, 204, 164, 0) 22.62%, #00cca4 99.93%), url("/tech-club-website/Event-1.png") lightgray -165.501px 0px / 224.563% 100.387%`,
 		backgroundSize: "cover",
 		backgroundPosition: "center",
 	};
