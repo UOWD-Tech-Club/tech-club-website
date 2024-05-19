52daf507c1888e8de0399c6df354ffb6aee10b3d
Windsmith <shriramsekar11@gmail.com>
2024-05-19 19:20:02 +0400

    feat: make footer and events section responsive

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,14 +1,17 @@
 import Hero from "../components/HeroSection/Hero";
 import Events from "../components/EventsSection/Events";
-import Newsfeed from "../components/NewsSection/Newsfeed";
+/*import Newsfeed from "../components/NewsSection/Newsfeed";*/
 
 function Homepage() {
 	return (
 		<>
 			<main>
 				<Hero />
+
 				<Events />
+				{/*
 				<Newsfeed />
+	*/}
 			</main>
 		</>
 	);
