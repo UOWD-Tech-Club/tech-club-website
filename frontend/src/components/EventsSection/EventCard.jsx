733d5ed316201cd79797d8ff9b44f043e6226c37
Windsmith <shriramsekar11@gmail.com>
2024-05-31 14:28:57 +0400

    fix: make event image dynamic again

--- frontend/src/components/EventsSection/EventCard.jsx
+++ frontend/src/components/EventsSection/EventCard.jsx
@@ -3,7 +3,7 @@ import CardTag from "../CardTag";
 
 function EventCard({ dateTime, title, img, tags }) {
 	const imageStyle = {
-		background: `linear-gradient(180deg, rgba(0, 204, 164, 0) 22.62%, #00cca4 99.93%), url("/tech-club-website/Event-1.png") lightgray -165.501px 0px / 224.563% 100.387%`,
+		background: `linear-gradient(180deg, rgba(0, 204, 164, 0) 22.62%, #00cca4 99.93%), url(${img}) lightgray -165.501px 0px / 224.563% 100.387%`,
 		backgroundSize: "cover",
 		backgroundPosition: "center",
 	};
