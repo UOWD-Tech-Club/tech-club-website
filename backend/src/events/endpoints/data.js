cbc263c9faa2fac878ef36f5f6c1eee18c2d1a7c
AeroArtz <70429569+AeroArtz@users.noreply.github.com>
2024-05-31 06:34:29 +0400

    Fixed Endpoints into data.js
    - Data now returned in endpoints including message about the event and the result returned from performing SQL Query
    
    -Added put endpoint

--- backend/src/events/endpoints/data.js
+++ backend/src/events/endpoints/data.js
@@ -10,23 +10,36 @@ var jsonParser = bodyParser.json()
 
 
 router.get('/', auth, async (req, res) => {
-    await getEvents();
-    res.send("test1");
+    let result  = await getEvents();
+    res.send('message':'events fetched', 'data': result });
 })
 
 router.get('/:eventId', auth, async (req, res) => {
     let id = req.params.eventId;
-    const row = await getEventById();
-    console.log(row);
-    res.send("test2" + id);
+    const result = await getEventById();
+    res.send({ 'message': 'event fetched', 'data': result })
 })
 
 router.post('/', jsonParser, auth, async (req, res) => {
     let event = req.body;
 
-    await createEvent(event);
+    let result = await createEvent(event);
 
-    res.send("test3");
+    res.send({ 'message': 'event inserted', 'data': result })
 })
+
+router.put('/:eventId', auth, jsonParser, async (req, res) => {
+    let id = req.params.eventId;
+    let event = req.body;
+
+    let result = await sql`update newsletter set ${sql(newsletter, 'newsletter_title', 'newsletter_body', 'tags', 'author', 'length', 'reading_time')
+        }
+    where newsletter_id = ${id} returning *`
+
+    console.log(result)
+
+    res.send({ 'message': 'events updated', 'data': result })
+})
+
 
 module.exports = router
