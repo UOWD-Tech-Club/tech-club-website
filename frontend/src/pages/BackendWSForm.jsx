942cb20e47788ab106021735bc3479dd331a6100
Shriram Sekar <41722013+shriram-sg@users.noreply.github.com>
2024-06-03 18:09:30 +0400

    Update BackendWSForm.jsx set language to english explicitly

--- frontend/src/pages/BackendWSForm.jsx
+++ frontend/src/pages/BackendWSForm.jsx
@@ -9,7 +9,7 @@ function BackendWSForm() {
                 <div><br /><br /><br /><br /><br /><br /></div>
                 <SocialLinks />
                 <div className="center">
-                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdlYhrd-w1Ms6lI6two9j34DJbEjZOZfv-0oZwniYdklO6IWA/viewform?embedded=true" width="100%" height="1200" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdlYhrd-w1Ms6lI6two9j34DJbEjZOZfv-0oZwniYdklO6IWA/viewform?embedded=true&hl=eo" width="100%" height="1200" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
                 </div>
             </main>
         </>
