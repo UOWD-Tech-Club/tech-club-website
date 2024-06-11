b0d244e818fdaf98e831a368126438bfb6913c12
AeroArtz <70429569+AeroArtz@users.noreply.github.com>
2024-06-11 09:38:22 +0400

    Update BackendWSForm.jsx
    moved title, description to the top and updated module.css file

--- frontend/src/pages/BackendWSForm.jsx
+++ frontend/src/pages/BackendWSForm.jsx
@@ -6,24 +6,18 @@ function BackendWSForm() {
     return (
         <>
             <main>
-                <div><br /><br /><br /><br /><br /><br /></div>
-                <SocialLinks />
-
-                <h1 style={{
-                    fontSize: "2rem",
-                    fontWeight: 800,
-
-                }}>
+                <h1>
                     Unlocking the Future: An Introduction to the Internet Computer Protocol
                 </h1>
 
-                <h4 style= {{
-                    fontSize: "1.5rem",
-                    fontWeight: 600,
-            
-                }}>
+                <h4>
                     "Unlocking the Future: An Introduction to the Internet Computer Protocol" is a dynamic training session designed for university students. They will be introduced to the fundamentals of blockchain technology and the innovative Internet Computer Protocol (ICP). Participants can expect to explore ICP’s architecture, engage in hands-on exercises, and discover its real-world applications. Ideal for all disciplines, no prior blockchain experience is required. They will gain insights from industry experts and enhance their knowledge for future career opportunities.
                 </h4>
+                
+                <div><br /><br /><br /><br /><br /><br /></div>
+                <SocialLinks />
+
+                
 
                 <div className="center">
                     <iframe width="640px" height="480px" src="https://forms.office.com/Pages/ResponsePage.aspx?id=fYm0qplfOUS0QsIExlh1tSZ78OXLGQREhBiLj9KzknxUQzFDT1dOWUgwRzMyQTQ2Wk44OEtSM0EyWS4u" frameborder="0" marginwidth="0" marginheight="0" style={{ border: "5px solid red;", width: "100%", height: "1300px", justifySelf: "center" }} allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>
